d69e8ddb1faf9dcf41b384dd55353c85ee307fb2
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-07-28 01:44:10 -0300

    project first structure and configs

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Reacrt from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './assets/styles/reset.css';
+import './assets/styles/style.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
